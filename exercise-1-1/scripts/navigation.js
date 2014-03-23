525fcd87ce1d4a59d5ef447c1d4a8b5bd00421dc
jakszy <jakszy@gmail.com>
2014-03-23 22:47:54 +0100

    Added swiping navigation
    Pages are looped in following order index->weekly->reviewers->index. To
    nawigate we are swiping left or right. I wanted to make two fingers
    swype instead of one finger swype, however... I don't if it is only iPad
    problem but two finger swyping works up/down/left but not right!! We
    cannot fully exclude triggering accidental page navigation action while
    moving panorama, but the times are configured in a way that it is not
    quite likely probable.

---
+++ exercise-1-1/scripts/navigation.js
@@ -0,0 +1,60 @@
+var forward;
+var backward;
+
+$.touch.triggerMouseEvents = true;
+$.touch.preventDefault = false;
+
+function setNavigationOrder(f,b){
+    forward = f;
+    backward = b;
+}
+
+$(document).touchable({
+    gesture: function (e, touchHistory) {
+        var touches = $(this).touches();
+        if (touches.length >= 2) {
+            var th = touchHistory.stop({
+                type: ['touchdown', 'touchup']
+            }).filter({
+                type: 'touchmove',
+                time: '1..100'
+            });
+            if (th.match([{ touch: touches[0], deltaX: '<-100' }, { touch: touches[1], deltaX: '<-100'}])) {
+                //message('two swipe left');
+            }
+            else if (th.match([{ touch: touches[0], deltaX: '>100' }, { touch: touches[1], deltaX: '>100'}])) {
+                //message('two swipe right');
+            }
+            else if (th.match([{ touch: touches[0], deltaY: '>100' }, { touch: touches[1], deltaY: '>100'}])) {
+                //message('two swipe down');
+            }
+            else if (th.match([{ touch: touches[0], deltaY: '<-100' }, { touch: touches[1], deltaY: '<-100'}])) {
+                //message('two swipe up');
+            }
+
+        } else {
+            var th = touchHistory.stop({
+                type: ['touchdown', 'touchup']
+            }).filter({
+                touch: touchHistory.get(0).touch,
+                type: 'touchmove',
+                time: '1..100'
+            });
+            if (th.match({ deltaX: '<-100' })) {
+                //message('one swipe left');
+                window.location.replace(backward);
+            } else if (th.match({ deltaX: '>100' })) {
+                //message('one swipe right');
+                window.location.replace(forward);
+            } else if (th.match({ deltaY: '>100' })) {
+                //message('one swipe down');
+            } else if (th.match({ deltaY: '<-100' })) {
+                //message('one swipe up');
+            }
+        }
+    }
+});
+
+//function message(s) {
+//    $('#message').html(s);
+//}
