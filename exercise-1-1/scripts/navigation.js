98ffd80b3085195d14c0e96f4e08ce9e7f4535ed
jakszy <jakszy@gmail.com>
2014-03-24 22:06:11 +0100

    Calibrated swiping + removed unused two finger gestures

--- exercise-1-1/scripts/navigation.js
+++ exercise-1-1/scripts/navigation.js
@@ -12,27 +12,7 @@ function setNavigationOrder(f,b){
 $(document).touchable({
     gesture: function (e, touchHistory) {
         var touches = $(this).touches();
-        if (touches.length >= 2) {
-            var th = touchHistory.stop({
-                type: ['touchdown', 'touchup']
-            }).filter({
-                type: 'touchmove',
-                time: '1..100'
-            });
-            if (th.match([{ touch: touches[0], deltaX: '<-100' }, { touch: touches[1], deltaX: '<-100'}])) {
-                //message('two swipe left');
-            }
-            else if (th.match([{ touch: touches[0], deltaX: '>100' }, { touch: touches[1], deltaX: '>100'}])) {
-                //message('two swipe right');
-            }
-            else if (th.match([{ touch: touches[0], deltaY: '>100' }, { touch: touches[1], deltaY: '>100'}])) {
-                //message('two swipe down');
-            }
-            else if (th.match([{ touch: touches[0], deltaY: '<-100' }, { touch: touches[1], deltaY: '<-100'}])) {
-                //message('two swipe up');
-            }
-
-        } else {
+        if (touches.length == 1) {
             var th = touchHistory.stop({
                 type: ['touchdown', 'touchup']
             }).filter({
@@ -40,16 +20,19 @@ $(document).touchable({
                 type: 'touchmove',
                 time: '1..100'
             });
-            if (th.match({ deltaX: '<-100' })) {
-                //message('one swipe left');
+            //left
+            if (th.match({ deltaX: '<-300' })) {
                 window.location.replace(backward);
-            } else if (th.match({ deltaX: '>100' })) {
-                //message('one swipe right');
+            //right
+            } else if (th.match({ deltaX: '>300' })) {
                 window.location.replace(forward);
+            //down
             } else if (th.match({ deltaY: '>100' })) {
-                //message('one swipe down');
+                //window.location.replace(forward);
+            //up
             } else if (th.match({ deltaY: '<-100' })) {
-                //message('one swipe up');
+                //window.location.replace(backward);
+                
             }
         }
     }
