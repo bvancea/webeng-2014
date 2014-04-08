154ab0440de85172c4bc68c864cec0c09f77a067
Bogdan Vancea <bogdan.aurel.vancea@gmail.com>
2014-04-08 21:55:27 +0200

    #3: Exercise 3 Displayed reviewers on the reviewer page.

---
+++ exercise-3-1/wp-content/themes/hotel/page-reviewers.php
@@ -0,0 +1,55 @@
+<?php
+
+get_header();
+?>
+<section id="container">
+
+    <section id="col-left">
+        <?php
+        get_sidebar('left');
+        ?>
+    </section>
+
+    <section id="col-center">
+        <div class="orange-box">
+            <div class="hotel-reviewers">
+                <?php
+                $loop = new WP_Query(
+                    array(
+                        'post_type' => 'reviewer',
+                        'posts_per_page' => -1,
+                        'meta_key' => 'reviewer_relevance',
+                        'orderby' => 'meta_value',
+                        'order' => 'ASC'
+                    )
+                );
+
+                while ( $loop->have_posts() ) : $loop->the_post();
+                    ?>
+                    <div class="reviewers-group">
+                        <article>
+                            <?php the_post_thumbnail('thumbnail')?>
+                            <span><?php the_title() ?></span>
+                            <div class="reviewer-info">
+                                <?php the_terms(get_the_ID(), 'reviewer_category', '<p>', '<br/>', '</p>') ?>
+
+                                <?php echo get_post_meta(get_the_ID(), 'reviewer_quote', TRUE); ?>
+                            </div>
+                        </article>
+                    </div>
+                <?php
+                endwhile;
+                ?>
+            </div>
+        </div>
+    </section>
+    <section id="col-right">
+        <?php
+        get_sidebar('right');
+        ?>
+    </section>
+</section>
+
+<?php
+get_footer();
+?>
