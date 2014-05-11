cf278e5f6c3a20a84d6461ebb7f47b0e23bfcd56
jakszy <jakszy@gmail.com>
2014-05-11 16:59:17 +0200

    Imrpoved group members management (adding/removing anytime)

--- exercise-4/app/controllers/groups_controller.rb
+++ exercise-4/app/controllers/groups_controller.rb
@@ -34,11 +34,18 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+    @user_ids=Membership.where(group_id: @group.id).pluck(:user_id)
+    @users=User.find(@user_ids)
   end
 
   def update
     @group = Group.find(params[:id])
     if @group.update_attributes(group_params)
+      Membership.where(group_id: @group.id).destroy_all
+      @user_ids=params[:user_ids]
+      @user_ids.each do |user_id|
+        Membership.create(user_id: user_id, group_id: @group.id)
+      end
       redirect_to action: 'index', controller: 'welcome'
     else
       render 'edit'
