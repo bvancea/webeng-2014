77dff405bb6898cb149e715edf9edbdecf2570d2
Bogdan Vancea <bogdan.aurel.vancea@gmail.com>
2014-05-15 22:19:33 +0200

    #4: Improved formatting.

--- exercise-4/test/controllers/login_controller_test.rb
+++ exercise-4/test/controllers/login_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class LoginControllerTest < ActionController::TestCase
-  test "should get login" do
+  test 'should get login' do
     get :login
     assert_response :success
   end
